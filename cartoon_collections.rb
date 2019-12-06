9bf5365b486a41ad8338ebe7a4bb473ddae1b347
Jazz Bozner <jazzbozner@gmail.com>
2019-12-06 00:05:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,5 @@
 def roll_call_dwarves# code an argument here
-  dwarfs.each_with_index do | name , index |
-    puts "#{index + 1} #{name}"
-  end
+  # Your code here
 end
 
 def summon_captain_planet# code an argument here
